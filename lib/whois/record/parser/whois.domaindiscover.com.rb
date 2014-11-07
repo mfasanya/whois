ad62f10a0ef9098482da3360b7d2009236d3f4dc
mfasanya <mitch@spinical.com>
2014-11-07 17:29:28 +0000

    Change to godaddy logic

--- lib/whois/record/parser/whois.domaindiscover.com.rb
+++ lib/whois/record/parser/whois.domaindiscover.com.rb
@@ -8,30 +8,24 @@
 
 
 require 'whois/record/parser/base'
-require 'whois/record/scanners/iana'
 
 
 module Whois
   class Record
     class Parser
 
-      # Parser for the whois.iana.org server.
+      # Parser for the whois.godaddy.com server.
+      #
+      # @see Whois::Record::Parser::Example
+      #   The Example parser for the list of all available methods.
+      #
       class WhoisDomaindiscoverCom < Base
-        include Scanners::Scannable
 
-        self.scanner = Scanners::Iana
+        property_not_supported :status
 
-
-        property_supported :status do
-          if available?
-            :available
-          else
-            :registered
-          end
-        end
-
+        # The server is contacted only in case of a registered domain.
         property_supported :available? do
-          !!(content_for_scanner =~ /This query returned 0 objects|organisation: Not assigned/)
+          false
         end
 
         property_supported :registered? do
@@ -39,61 +33,76 @@ module Whois
         end
 
 
-        # TODO: registrar
-
-        property_supported :registrant_contacts do
-          build_contact("organisation", Whois::Record::Contact::TYPE_REGISTRANT)
-        end
-
-        property_supported :admin_contacts do
-          build_contact("administrative", Whois::Record::Contact::TYPE_ADMINISTRATIVE)
-        end
-
-        property_supported :technical_contacts do
-          build_contact("technical", Whois::Record::Contact::TYPE_TECHNICAL)
-        end
-
-
         property_supported :created_on do
-          node("dates") { |raw| Time.parse(raw["created"]) if raw.has_key? "created" }
+          if content_for_scanner =~ /Creation Date: (.+)\n/
+            Time.parse($1)
+          end
         end
-
+        
         property_supported :updated_on do
-          node("dates") { |raw| Time.parse(raw["changed"]) if raw.has_key? "changed" }
+          if content_for_scanner =~ /Updated* Date: (.+)\n/
+            Time.parse($1)
+          end
         end
-
-        property_not_supported :expires_on
-
-
-        property_supported :nameservers do
-          node("nameservers") do |raw|
-            (raw["nserver"] || "").split("\n").map do |line|
-              name, ipv4 = line.downcase.split(/\s+/)
-              Record::Nameserver.new(:name => name, :ipv4 => ipv4)
-            end
+        
+        property_supported :expires_on do
+          if content_for_scanner =~ /Expiration Date: (.+)\n/
+            Time.parse($1)
           end
         end
 
 
+        property_supported :registrar do
+          Record::Registrar.new(
+              name:         content_for_scanner[/Registrar: (.+)\n/, 1],
+              url:          content_for_scanner[/Registrar URL: (.+)\n/, 1],
+          )
+        end
+
+        property_supported :registrant_contacts do
+          build_contact('Registrant', Record::Contact::TYPE_REGISTRANT)
+        end
+
+        property_supported :admin_contacts do
+          build_contact('Admin', Record::Contact::TYPE_ADMINISTRATIVE)
+        end
+
+        property_supported :technical_contacts do
+          build_contact('Tech', Record::Contact::TYPE_TECHNICAL)
+        end
+
+        property_supported :nameservers do
+          content_for_scanner.scan(/Name Server: (.+)\n/).map do |line|
+            Record::Nameserver.new(name: line[0].strip)
+          end
+        end
+
       private
 
         def build_contact(element, type)
-          node(element) do |raw|
-            if raw["organisation"] != "Not assigned"
-              address = (raw["address"] || "").split("\n")
-              Record::Contact.new(
-                :type         => type,
-                :name         => raw["name"],
-                :organization => raw["organisation"],
-                :address      => address[0],
-                :city         => address[1],
-                :zip          => address[2],
-                :country      => address[3],
-                :phone        => raw["phone"],
-                :fax          => raw["fax-no"],
-                :email        => raw["e-mail"]
-              )
-            end
+          Record::Contact.new(
+              type:         type,
+              id:           nil,
+              name:         value_for_property(element, 'Name'),
+              organization: value_for_property(element, 'Organization'),
+              address:      value_for_property(element, 'Street'),
+              city:         value_for_property(element, 'City'),
+              zip:          value_for_property(element, 'Postal Code'),
+              state:        value_for_property(element, 'State/Province'),
+              country:      value_for_property(element, 'Country'),
+              phone:        value_for_property(element, 'Phone'),
+              fax:          value_for_property(element, 'Fax'),
+              email:        value_for_property(element, 'Email')
+          )
+        end
+
+        def value_for_property(element, property)
+          matches = content_for_scanner.scan(/#{element} #{property}:\s(.+)\n/)
+          value = matches.collect(&:first).join(', ')
+          if value == ""
+            nil
+          else
+            value
           end
         end
 
