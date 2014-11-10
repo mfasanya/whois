6b15ea70a422e75ee6b28d2ab4aa9800be5a26bb
mfasanya <mitch@spinical.com>
2014-11-10 00:26:59 +0000

    Add Networksolutions throttle

---
+++ lib/whois/record/scanners/whois.networksolutions.com.rb
@@ -0,0 +1,34 @@
+#--
+# Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+# Copyright (c) 2009-2014 Simone Carletti <weppos@weppos.net>
+#++
+
+
+require 'whois/record/scanners/base_whoisd'
+
+
+module Whois
+  class Record
+    module Scanners
+
+      class WhoisNetworksolutionsCom < BaseWhoisd
+
+        self.tokenizers += [
+            :scan_response_throttled,
+        ]
+
+        tokenizer :scan_response_throttled do
+          if @input.match?(/contained within a list of IP addresses that may have failed/)
+            @ast["response:throttled"] = true
+            @input.skip(/^.+\n/)
+          end
+        end
+
+      end
+
+    end
+  end
+end
