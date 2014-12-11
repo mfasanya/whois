f96104d552680d649f194bffad27a846635c4af1
mfasanya <mitch@spinical.com>
2014-12-11 00:54:17 +0000

    Update 2 line address

--- lib/whois/record/parser/whois.apnic.net.rb
+++ lib/whois/record/parser/whois.apnic.net.rb
@@ -62,91 +62,29 @@ module Whois
           address = nil
           country = nil
           country_code = nil
+          city = nil
 
           if select[1]
             count = 0
             data = select[1][0]
             
-            data.scan(/address: (.+?)\n/).flatten.map do |line|
-              if count == 0
-                name = line.strip
-              elsif count == 1
-                address = line.strip
-              elsif count == 2
-                country = line.strip
-              end
-              count += 1
-            end
+            arr = data.scan(/address: (.+?)\n/)
 
-            if data =~ /abuse-mailbox: (.+?)\n/
-              email = $1.strip
-            end
-
-            if data =~ /e-mail: (.+?)\n/
-              email = $1.strip
-            end
-
-            if data =~ /phone: (.+?)\n/
-              phone = $1.strip
-            end
-
-            if data =~ /fax-no: (.+?)\n/
-              fax = $1.strip
-            end
-
-            if data =~ /person: (.+?)\n/
-              name = $1.strip
-            end
-
-            if data =~ /country: (.+?)\n/
-              country_code = $1.strip
-            end
-
-            if content_for_scanner =~ /descr: (.+?)\n/
-              organization = $1.strip
-            end
-
-            Record::Contact.new(
-              type:         Record::Contact::TYPE_ADMINISTRATIVE,
-              id:           nil,
-              organization: organization,
-              name:         name,
-              address:      address,
-              country:      country,
-              country_code: country_code,
-              email:        email,
-              phone:        phone,
-              fax:          fax, 
-            )
-          end
-        end
-
-        property_supported :technical_contacts do
-          select = content_for_scanner.scan(/\n\n((.|\n)*?)source:/)
-
-          email = nil
-          phone = nil
-          fax = nil
-          name = nil
-          organization = nil
-          name = nil
-          address = nil
-          country = nil
-          country_code = nil
-
-          print select
-
-          if select[2]
-            count = 0
-            data = select[2][0]
-
-            data.scan(/address: (.+?)\n/).flatten.map do |line|
-              if count == 1
-                name = line.strip
-              elsif count == 2
-                address = line.strip
-              elsif count == 3
-                country = line.strip
+            arr.flatten.map do |line|
+              if arr.length == 2
+                if count == 0
+                  address = line.strip
+                elsif count == 1
+                  city = line.strip
+                end
+              elsif arr.length == 3
+                if count == 0
+                  name = line.strip
+                elsif count == 1
+                  address = line.strip
+                elsif count == 2
+                  country = line.strip
+                end
               end
               count += 1
             end
@@ -189,6 +127,90 @@ module Whois
               country_code: country_code,
               email:        email,
               phone:        phone,
+              city:         city,
+              fax:          fax, 
+            )
+          end
+        end
+
+        property_supported :technical_contacts do
+          select = content_for_scanner.scan(/\n\n((.|\n)*?)source:/)
+
+          email = nil
+          phone = nil
+          fax = nil
+          name = nil
+          organization = nil
+          name = nil
+          address = nil
+          country = nil
+          country_code = nil
+          city = nil
+
+          if select[2]
+            count = 0
+            data = select[2][0]
+
+            arr = data.scan(/address: (.+?)\n/)
+
+            arr.flatten.map do |line|
+              if arr.length == 2
+                if count == 0
+                  address = line.strip
+                elsif count == 1
+                  city = line.strip
+                end
+              elsif arr.length == 3
+                if count == 0
+                  name = line.strip
+                elsif count == 1
+                  address = line.strip
+                elsif count == 2
+                  country = line.strip
+                end
+              end
+              count += 1
+            end
+
+            if data =~ /abuse-mailbox: (.+?)\n/
+              email = $1.strip
+            end
+
+            if data =~ /e-mail: (.+?)\n/
+              email = $1.strip
+            end
+
+            if data =~ /phone: (.+?)\n/
+              phone = $1.strip
+            end
+
+            if data =~ /fax-no: (.+?)\n/
+              fax = $1.strip
+            end
+
+            if data =~ /person: (.+?)\n/
+              name = $1.strip
+            end
+
+            if data =~ /country: (.+?)\n/
+              country_code = $1.strip
+            end
+
+            if content_for_scanner =~ /descr: (.+?)\n/
+              organization = $1.strip
+            end
+
+            Record::Contact.new(
+              type:         Record::Contact::TYPE_TECHNICAL,
+              id:           nil,
+              organization: organization,
+              name:         name,
+              address:      address,
+              country:      country,
+              country_code: country_code,
+              email:        email,
+              phone:        phone,
+              city:         city,
               fax:          fax, 
             )
           end
